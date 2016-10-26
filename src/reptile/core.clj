5b0bdacafa170a5c0fc5a6331d42e7176505ff36
iku000888 <kanumaiku@gmail.com>
2016-10-26 13:24:27 +0900

    Use clojure 1.9 and protocolize some fns

--- src/reptile/core.clj
+++ src/reptile/core.clj
@@ -1,8 +1,24 @@
 (ns reptile.core
+  (:require [com.stuartsierra.component :as component])
   (:import [javax.swing JFrame JLabel SwingUtilities]))
 
-(defn start []
-  (let [frame (JFrame. "HelloWorldSwing")
-        label (JLabel. "Hello World")]
-    (-> frame .getContentPane (.add label))
-    (doto frame (.setDefaultCloseOperation JFrame/DISPOSE_ON_CLOSE) .pack (.setVisible true))))
+(defprotocol IReptileJFrame
+  (start [frame])
+  (stop [frame])
+  (set-width-height [frame w h]))
+
+(defrecord ReptileJFrame [map]
+  IReptileJFrame
+  (start [this]
+    (let [frame (JFrame. "HelloWorldSwing")
+          label (JLabel. "Hello World")]
+      (-> frame .getContentPane (.add label))
+      (doto frame (.setDefaultCloseOperation JFrame/DISPOSE_ON_CLOSE)
+            .pack
+            (.setVisible true))
+      (assoc this :frame frame)))
+  (stop [this]
+    (.dispose (:frame this))
+    (dissoc this :frame))
+  (set-width-height [this w h]
+    (.setSize (:frame this) w h)))
